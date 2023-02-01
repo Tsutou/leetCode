d33084034e0614be77c85fad764ee00b2c9f811b
tsutou <tsutoutakehara@gmail.com>
2023-02-01 23:45:31 +0900

    solve Median of Two Sorted Arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,5 +8,18 @@ fun main(args: Array<String>) {
 
 class Solution {
 
-
+    // 4. Median of Two Sorted Arrays
+    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+        val mergedArray = nums1 + nums2
+        mergedArray.sort()
+        val totalSize = mergedArray.size
+        val isOdd = totalSize % 2 != 0
+        return if (isOdd) {
+            val midIndex = (totalSize - 1) / 2
+            mergedArray[midIndex].toDouble()
+        } else {
+            val midIndex = totalSize / 2
+            (mergedArray[midIndex -1] + mergedArray[midIndex]) / 2.toDouble()
+        }
+    }
 }
